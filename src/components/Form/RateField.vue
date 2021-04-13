bd238f2256759b3cfdbc81d104dbf8fcee55737b
hieu <hieu.tran@gumiviet.com>
2021-04-13 16:51:14 +0700

    feat(update) : finish building rating star

---
+++ src/components/Form/RateField.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="switch">
+    <!--Label-->
+    <label
+      v-if="label"
+      class="label"
+      v-text="label" />
+
+    <!--Field-->
+    <div>
+      <el-rate
+        :allow-half="allowHalf"
+        :void-icon-class="voidIconClass"
+        :icon-classes="iconClasses"
+        :show-text="showText"
+        :value="value"
+        :disabled="disabled"
+        :colors="colors"
+        :texts="texts"
+        :disabled-void-color="disabledVoidColor"
+        :max="max"
+        @change="onChange" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RateField',
+
+  model: {
+    prop: 'value',
+    event: 'change'
+  },
+
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+
+    value: {
+      type: Number,
+      default: null
+    },
+
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+
+    disabledVoidColor: {
+      type: String,
+      default: ''
+    },
+
+    colors: {
+      type: Array,
+      default: undefined
+    },
+
+    showText: {
+      type: Boolean,
+      default: false
+    },
+
+    texts: {
+      type: Array,
+      default: undefined
+    },
+
+    iconClasses: {
+      type: Array,
+      default: undefined
+    },
+
+    voidIconClass: {
+      type: String,
+      default: undefined
+    },
+
+    allowHalf: {
+      type: Boolean,
+      default: false
+    },
+
+    max: {
+      type: Number,
+      default: 5
+    }
+  },
+
+  methods: {
+    onChange (e) {
+      this.$emit('change', e)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/ElementUI/ElementRateColor.vue
+++ src/views/ElementUI/ElementRateColor.vue
@@ -4,7 +4,66 @@
       <div class="row">
         <!--Rate-->
         <div class="col-md-4">
-          Rate
+          <!--Rate default-->
+          <RateField
+            v-model="rate.default"
+            class="mb-3"
+            :label="'Rate default'"
+            :field="'Rate default'" />
+
+          <!--Rate disable-->
+          <RateField
+            v-model="rate.disable"
+            class="mb-3"
+            disabled-void-color="#d4d4d4"
+            :disabled="true"
+            :label="'Rate disable'"
+            :field="'Rate disable'" />
+
+          <!--Rate different levels-->
+          <RateField
+            v-model="rate.set_color_level"
+            class="mb-3"
+            :colors="['#99A9BF', '#F7BA2A', '#FF9900']"
+            :label="'Different levels'"
+            :field="'Different levels'" />
+
+          <!--Rate show text-->
+          <RateField
+            v-model="rate.set_text"
+            class="mb-3"
+            :colors="['#99A9BF', '#F7BA2A', '#FF9900']"
+            :show-text="true"
+            :texts="['oops', 'disappointed', 'normal', 'good', 'great']"
+            :label="'Show text'"
+            :field="'Show text'" />
+
+          <!--Rate with icon-->
+          <RateField
+            v-model="rate.set_icon"
+            class="mb-3"
+            void-icon-class="el-icon-remove"
+            :icon-classes="['el-icon-remove', 'el-icon-circle-plus', 'el-icon-success']"
+            :colors="['#99A9BF', '#F7BA2A', '#FF9900']"
+            :label="'Rate icon'"
+            :field="'Rate icon'" />
+
+          <!--Rate half-->
+          <RateField
+            v-model="rate.half"
+            class="mb-3"
+            :allow-half="true"
+            :colors="['#99A9BF', '#F7BA2A', '#FF9900']"
+            :label="'Rate half'"
+            :field="'Rate half'" />
+
+          <!--Rate limit-->
+          <RateField
+            v-model="rate.limit"
+            class="mb-3"
+            :max="7"
+            :label="'Rate limit'"
+            :field="'Rate limit'" />
         </div>
 
         <!--Color picker-->
@@ -57,13 +116,24 @@
 
 <script>
 
+import RateField from '@/components/Form/RateField'
 export default {
   name: 'ElementRateColor',
+
+  components: {
+    RateField
+  },
 
   data () {
     return {
       rate: {
-        default: ''
+        default: null,
+        disable: null,
+        set_color_level: null,
+        set_text: null,
+        set_icon: null,
+        half: null,
+        limit: null
       },
       color_picker: {
         default: ''
